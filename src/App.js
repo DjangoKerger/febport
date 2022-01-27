75d460e215c1617f243d2d8818ee1ba77013e72d
“First <Kerger06@aol.com>
2022-01-27 11:36:36 -0600

    started on components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Home from './Components/Home';
+import About from './Components/About';
+import SinglePost from './Components/SinglePost';
+import Post from './Components/Post';
+import Project from './Components/Project'
+
+
+// 1 import browser router route and switch from react router dom.
+// 2 then place browser router with switch inside and the routes we need for each component.
+// 3 the route has a component its pointing towards and a path that it will follow in the webbrowser.
+// 4 when a route has the word of another route and more be sure to place that one with the least words on the bottom.
+//import all components used.
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return (<BrowserRouter>
+            <Switch>
+              <Route component={Home} path='/'></Route>
+              <Route component={About} path='/about'></Route>
+              <Route component={SinglePost} path='/post/:slug'></Route>
+              <Route component={Post} path='/post'></Route>
+              <Route component={Project} path='/projecy'></Route>
+            </Switch>
+          </BrowserRouter>)
 }
 
 export default App;
